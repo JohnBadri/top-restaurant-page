293642970d4899c5529e2ff9443d3b1d4d2803d7
JohnBadri <135007003+JohnBadri@users.noreply.github.com>
2023-10-29 09:49:46 +1100

    Final Product - The Odin Project - Restaurant Page

--- src/js/contact.js
+++ src/js/contact.js
@@ -1,4 +1,31 @@
 export const contactSection = () => {
   const section = document.createElement("section");
+  section.classList.add("hero-image-contact");
+
+  const heroContainer = document.createElement("div");
+  heroContainer.classList.add("hero-container");
+  section.appendChild(heroContainer);
+
+  const contentContainer = document.createElement("div");
+  contentContainer.classList.add("content-container");
+  heroContainer.appendChild(contentContainer);
+
+  const subHeading = document.createElement("h3");
+  subHeading.classList.add("hero-content");
+  subHeading.textContent =
+    "Whiskey & Cigars: Elevate Your Moments, Savor the Experience";
+  contentContainer.appendChild(subHeading);
+
+  const heading = document.createElement("h2");
+  heading.classList.add("hero-content");
+  heading.textContent = "The Temple Bar";
+  contentContainer.appendChild(heading);
+
+  const button = document.createElement("button");
+  button.classList.add("hero-content", "hero-button");
+  button.type = "button";
+  button.textContent = "Bit lazy to create the form - goes back to menu";
+  contentContainer.appendChild(button);
+
   return section;
 };
